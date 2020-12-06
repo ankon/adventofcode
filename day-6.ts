a3a1897e3732f984c5c064567d02b0e7149b869b
Andreas Kohn <andreas.kohn@gmail.com>
2020-12-06 11:46:12 +0100

    Fix up indentation

--- day-6.ts
+++ day-6.ts
@@ -14,25 +14,25 @@ let currentPart1 = new Set();
 let currentPart2 = new Set(ALL);
 
 rl.on('line', line => {
-    if (line.trim()) {
-        // More answers for the current group
-        Array.from(line).forEach(answer => currentPart1.add(answer));
-        for (const answer of currentPart2.values()) {
-            if (line.indexOf(answer) === -1) {
-                currentPart2.delete(answer);
-            }
-        }
-    } else {
-        // Group change, count and add
-        resultPart1 += currentPart1.size;
-        currentPart1.clear();
-        resultPart2 += currentPart2.size;
-        ALL.forEach(answer => currentPart2.add(answer));
-    }
+	if (line.trim()) {
+		// More answers for the current group
+		Array.from(line).forEach(answer => currentPart1.add(answer));
+		for (const answer of currentPart2.values()) {
+			if (line.indexOf(answer) === -1) {
+				currentPart2.delete(answer);
+			}
+		}
+	} else {
+		// Group change, count and add
+		resultPart1 += currentPart1.size;
+		currentPart1.clear();
+		resultPart2 += currentPart2.size;
+		ALL.forEach(answer => currentPart2.add(answer));
+	}
 });
 rl.on('close', () => {
-    resultPart1 += currentPart1.size;
-    resultPart2 += currentPart2.size;
+	resultPart1 += currentPart1.size;
+	resultPart2 += currentPart2.size;
 
-    console.log(`Got in total part1 ${resultPart1} part2 ${resultPart2}`);
+	console.log(`Got in total part1 ${resultPart1} part2 ${resultPart2}`);
 });
