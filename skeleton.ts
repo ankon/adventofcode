0f3118b50133712d6a96800330abca576aefc6d6
Andreas Kohn <andreas.kohn@gmail.com>
2020-12-07 13:18:54 +0100

    Improve the skeleton to make it easier to handle multiple inputs

--- skeleton.ts
+++ skeleton.ts
@@ -4,9 +4,16 @@ import { createReadStream } from 'fs';
 import { basename, extname } from 'path';
 import { createInterface } from 'readline';
 
-const rl = createInterface(createReadStream(`${basename(process.argv[1], extname(process.argv[1]))}.input`));
+function processInput(input: string) {
+	const rl = createInterface(createReadStream(input));
 
-rl.on('line', line => {
-});
-rl.on('close', () => {
-});
+	rl.on('line', line => {
+		// Process `line` here
+	});
+	rl.on('close', () => {
+		console.log(`Results for ${input}:`);
+		// Report results for this input
+	});
+}
+
+processInput(`${basename(process.argv[1], extname(process.argv[1]))}.input`);
