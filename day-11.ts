767eae166e6fb6c0b5527518e96f70c2947c4cc1
Andreas Kohn <andreas.kohn@gmail.com>
2020-12-13 16:31:36 +0100

    Add day 11 part 2

--- day-11.ts
+++ day-11.ts
@@ -27,6 +27,22 @@ function getNeighborOccupancyPart1(field: State[][], i: number, j: number): numb
 	}, 0);
 }
 
+function getNeighborOccupancyPart2(field: State[][], i: number, j: number): number {
+	let count = 0;
+	for (const [di, dj] of adjacency) {
+		let ni = i + di, nj = j + dj;
+		while (between(ni, 0, field.length - 1) && between(nj, 0, field[ni].length - 1) && field[ni][nj] === '.') {
+			ni += di;
+			nj += dj;
+		}
+
+		if (between(ni, 0, field.length - 1) && between(nj, 0, field[ni].length - 1) && field[ni][nj] === '#') {
+			count++;
+		}
+	}
+	return count;
+}
+
 function iterate(field: State[][], getNeighborOccupancy: GetNeighborOccupancy, dieAtOrAbove: number): { newField: State[][], changes: number } {
 	const newField: State[][] = [];
 	let changes = 0;
@@ -56,10 +72,18 @@ function iterate(field: State[][], getNeighborOccupancy: GetNeighborOccupancy, d
 	return {newField, changes};
 }
 
+function logField(field: State[][]) {
+	field.forEach(row => console.log(row.join('')));
+}
+
 function processInput(input: string) {
 	const rl = createInterface(createReadStream(input));
 
 	const initialField: State[][] = [];
+	const cases = [
+		{ getNeighborOccupancy: getNeighborOccupancyPart1, dieAtOrAbove: 4 },
+		{ getNeighborOccupancy: getNeighborOccupancyPart2, dieAtOrAbove: 5 },
+	];
 
 	rl.on('line', line => {
 		// Process `line` here
@@ -67,20 +91,23 @@ function processInput(input: string) {
 	});
 	rl.on('close', () => {
 		console.log(`Results for ${input}:`);
-		let round = 1;
-		let field = initialField;
-		let result;
-		do {
-			result = iterate(field, getNeighborOccupancyPart1, 4);
-			field = result.newField;
-			// field.forEach(row => console.log(row.join('')));
-			console.log(`Changes after round ${round}: ${result.changes}`);
-			round++;
-		} while (result.changes > 0);
-		
-		// Count the number of occupied seats
-		const seats = field.map(row => row.reduce((cnt, state) => cnt + (state === '#' ? 1 : 0), 0)).reduce((cnt, rowCount) => cnt + rowCount, 0);
-		console.log(`Occupied: ${seats}`);
+		for (const {getNeighborOccupancy, dieAtOrAbove} of cases) {
+			let round = 1;
+			let field = initialField;
+			let result;
+			logField(initialField);
+			do {
+				result = iterate(field, getNeighborOccupancy, dieAtOrAbove);
+				field = result.newField;
+				logField(field);
+				console.log(`Changes after round ${round}: ${result.changes}`);
+				round++;
+			} while (result.changes > 0);
+
+			// Count the number of occupied seats
+			const seats = field.map(row => row.reduce((cnt, state) => cnt + (state === '#' ? 1 : 0), 0)).reduce((cnt, rowCount) => cnt + rowCount, 0);
+			console.log(`Occupied ${getNeighborOccupancy.name}: ${seats}`);
+		}
 	});
 }
 
