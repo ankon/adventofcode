dc14af374830e61086500afe7eee099a37141b99
Andreas Kohn <andreas.kohn@gmail.com>
2021-12-02 12:21:21 +0100

    Process the input files "one after the other"

--- skeleton.ts
+++ skeleton.ts
@@ -4,23 +4,34 @@ import { createReadStream } from 'fs';
 import { basename, extname } from 'path';
 import { createInterface } from 'readline';
 
-function processInput(input: string) {
+function processInput(input: string): Promise<void> {
 	const rl = createInterface(createReadStream(input));
 
-	rl.on('line', line => {
-		// Process `line` here
+	return new Promise((resolve, reject) => {
+		rl.on('line', line => {
+			// Process `line` here
+		});
+		rl.on('error', err => {
+			reject(err);
+		});
+		rl.on('close', () => {
+			console.log(`Results for ${input}:`);
+			// Report results for this input
+
+			resolve();
+		});
 	});
-	rl.on('close', () => {
-		console.log(`Results for ${input}:`);
-		// Report results for this input
-	});
+}
+
+async function main(inputFiles: string[]) {
+	for (const inputFile of inputFiles) {
+		await processInput(inputFile);
+	}
 }
 
 const INPUT_SPECS = [
 	'-example',
 	'',
 ];
-for (const inputSpec of INPUT_SPECS) {
-	const inputFile = `${basename(process.argv[1], extname(process.argv[1]))}${inputSpec}.input`;
-	processInput(inputFile);
-}
+
+main(INPUT_SPECS.map(inputSpec => `${basename(process.argv[1], extname(process.argv[1]))}${inputSpec}.input`)).catch(err => console.error(err));
