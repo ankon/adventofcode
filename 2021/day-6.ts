61a506ba01bb02886660479001f25792cd91ba43
Andreas Kohn <andreas.kohn@gmail.com>
2021-12-06 09:38:56 +0100

    Implement day 6 part 2

--- 2021/day-6.ts
+++ 2021/day-6.ts
@@ -4,7 +4,14 @@ import { createReadStream } from 'fs';
 import { basename, extname } from 'path';
 import { createInterface } from 'readline';
 
-function simulate(ages: number[], days: number): number {
+/**
+ * Simulate the fish "naively"
+ *
+ * @param ages
+ * @param days
+ * @returns
+ */
+function simulateBruteForce(ages: number[], days: number): number {
 	let state = ages;
 	for (let i = 0; i < days; i++) {
 		state = state.reduce((r, age) => {
@@ -22,11 +29,36 @@ function simulate(ages: number[], days: number): number {
 	return state.length;
 }
 
+/**
+ * Simulate the fish using age buckets
+ *
+ * @param ages
+ * @param days
+ */
+function simulateWithBuckets(ages: number[], days: number): number {
+	/** Age buckets: Number of fish per age group */
+	const buckets: number[] = ages.reduce((r, age) => {
+		r[age]++;
+		return r;
+	}, new Array(9).fill(0));
+
+	for (let i = 0; i < days; i++) {
+		// Shift the array left, and create new fish for the number of fish
+		// on age 0
+		const [newParents] = buckets.splice(0, 1);
+		// Parents start back at 6
+		buckets[6] += newParents;
+		// ... and the newly spawned fish pop up at the end (which we just shifted away, so we
+		// just need to push that back there).
+		buckets.push(newParents);
+	}
+
+	// Add up all the counts to come to the number of fish
+	return buckets.reduce((r, count) => r + count, 0);
+}
+
 function processInput(input: string): Promise<void> {
 	const rl = createInterface(createReadStream(input));
-
-	/** Days to simulate */
-	const DAYS = 80;
 
 	/** Ages of all fish ("state") */
 	let ages: number[];
@@ -39,8 +71,10 @@ function processInput(input: string): Promise<void> {
 			reject(err);
 		});
 		rl.on('close', () => {
-			const fishAtEnd = simulate(ages, DAYS);
-			console.log(`Results for ${input}: ${fishAtEnd} fish after ${DAYS} days`);
+			// XXX: Use both functions to not get complaints about unused code
+			const fishAtEndPart1 = simulateBruteForce(ages, 80);
+			const fishAtEndPart2 = simulateWithBuckets(ages, 256);
+			console.log(`Results for ${input}: ${fishAtEndPart1} fish after 80 days, ${fishAtEndPart2} after 256 days`);
 
 			resolve();
 		});
