656d434e05b337723f01d315e71da10df288f6e4
Andreas Kohn <andreas.kohn@gmail.com>
2022-12-06 09:06:38 +0100

    Day 6, Task 2: Longer signals
    This seems to mark the inflection point, where we will work on a bigger puzzle over
    multiple days. There will be more refactoring in the framework ...

--- days/6/days6.go
+++ days/6/days6.go
@@ -5,12 +5,23 @@ import (
 	"fmt"
 )
 
-var knownInputs = map[string]int{
-	"mjqjpqmgbljsphdztnvjfqwrcgsmlb": 7,
-	// "bvwbjplbgvbhsrlpgdmjqwftvncz": 5,
-	// "nppdvjthqldpwncqszvftbrmjlhg": 6,
-	// "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg": 10,
-	// "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw": 11,
+type expected struct {
+	packet int
+	message int
+}
+
+type requiredLength int
+const (
+	startOfPacket requiredLength  = 4
+	startOfMessage requiredLength = 14
+)
+
+var knownInputs = map[string]expected{
+	"mjqjpqmgbljsphdztnvjfqwrcgsmlb": {7, 19},
+	"bvwbjplbgvbhsrlpgdmjqwftvncz": {5, 23},
+	"nppdvjthqldpwncqszvftbrmjlhg": {6, 23},
+	"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg": {10, 29},
+	"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw": {11, 26},
 }
 
 //go:embed input.txt
@@ -18,23 +29,36 @@ var fullInput string
 
 func Run(useSampleInput bool) error {
 	for input, expectedResult := range knownInputs {
-		result, err := detectMarker(input)
+		result, err := detectMarker(input, startOfPacket)
 		if err != nil {
 			return fmt.Errorf("tests failed: %w", err)
 		}
-		if result != expectedResult {
-			return fmt.Errorf("unexpected result for %q: Expected %d, got %d", input, expectedResult, result)
+		if result != expectedResult.packet {
+			return fmt.Errorf("unexpected start-of-packet marker for %q: Expected %d, got %d", input, expectedResult.packet, result)
+		}
+		result, err = detectMarker(input, startOfMessage)
+		if err != nil {
+			return fmt.Errorf("tests failed: %w", err)
+		}
+		if result != expectedResult.message {
+			return fmt.Errorf("unexpected start-of-message marker for %q: Expected %d, got %d", input, expectedResult.message, result)
 		}
 	}
 	if useSampleInput {
 		return nil
 	}
 
-	index, err := detectMarker(fullInput)
+	index, err := detectMarker(fullInput, startOfPacket)
 	if err != nil {
-		return fmt.Errorf("failed to detect marker: %w", err)
+		return fmt.Errorf("failed to detect start-of-packet marker: %w", err)
 	}
-	fmt.Printf("Marker after %d\n", index)
+	fmt.Printf("start-of-packet marker after %d\n", index)
+
+	index, err = detectMarker(fullInput, startOfMessage)
+	if err != nil {
+		return fmt.Errorf("failed to detect start-of-message marker: %w", err)
+	}
+	fmt.Printf("start-of-message marker after %d\n", index)
 
 	return nil
 }
@@ -50,9 +74,9 @@ func isMarker(s string) bool {
 	return true
 }
 
-func detectMarker(input string) (int, error) {
-	for i := 4; i < len(input); i++ {
-		if isMarker(input[i-4:i]) {
+func detectMarker(input string, t requiredLength) (int, error) {
+	for i := int(t); i < len(input); i++ {
+		if isMarker(input[i-int(t):i]) {
 			return i, nil
 		}
 	}
