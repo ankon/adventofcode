f474cc0d748134c503eedb0200e07e79567fd8c3
Andreas Kohn <andreas.kohn@gmail.com>
2023-12-02 20:50:13 +0100

    Part 2: More tests, more structure, more rust.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-const TEST_STR: &str = "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet";
-
 #[derive(Debug)]
 struct Line {
-    digits: Vec<i32>,
+    digits: Vec<u32>,
 }
 
 impl Line {
@@ -15,20 +10,32 @@ impl Line {
         }
     }
 
-    pub fn visit_digit(&mut self, digit: i32) {
-        if self.digits.len() == 2 {
-            self.digits.pop();
-        }
+    pub fn visit_digit(&mut self, digit: u32) {
         self.digits.push(digit);
     }
 
-    pub fn value(&self) -> i32 {
+    pub fn value(&self) -> u32 {
         let first = self.digits.first().unwrap();
-        if let Some(last) = self.digits.get(1) {
-            10 * first + last
-        } else {
-            10 * first + first
+        let last = self.digits.last().unwrap();
+        10 * first + last
+    }
+
+    fn match_digit_word_prefix(s: &str, at: usize) -> Option<(u32, usize)> {
+        static DIGIT_WORDS: [&str; 9] = [ /* "zero", */ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+
+        let mut result = None;
+        for (digit, word) in DIGIT_WORDS.iter().enumerate() {
+            // println!("word = {}, word.len = {}, digit = {}, at = {}, s.len = {}", word, word.len(), digit, at, s.len());
+            // println!("s.get = {:?}", s.get(at..at+word.len()).unwrap());
+            if let Some(w) = s.get(at..at+word.len()) {
+                // println!("w = {}, word = {}", w, *word);
+                if w == *word {
+                    result = Some(((digit + 1) as u32, word.len()));
+                    break;
+                }
+            }
         }
+        result
     }
 }
 
@@ -36,38 +43,48 @@ impl std::str::FromStr for Line {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut i = 0;
         let mut line = Line::new();
-        for c in s.chars() {
-            if let '0'..='9' = c {
-                let digit = c.to_digit(10).unwrap();
-                line.visit_digit(digit as i32);
+        while i < s.len() {
+            if let Some((digit, _len)) = Line::match_digit_word_prefix(s, i) {
+                line.visit_digit(digit);
+                // oneight...
+                i += 1;
+            } else {
+                let sl = s.get(i..i+1).unwrap();
+                // println!("sl = {}, i = {}", sl, i);
+                let c = sl.chars().next().unwrap();
+                if let Some(digit) = c.to_digit(10) {
+                    line.visit_digit(digit);
+                }
+                i += 1;
             }
         }
         Ok(line)
     }
 }
 
-fn calculate_calibration_value(input: &str) -> i32 {
-    let mut result = 0;
-    for line in input.lines() {
+fn calculate_calibration_value(input: &str) -> u32 {
+    let mut result = [].to_vec();
+    for (index, line) in input.lines().enumerate() {
         let parsed_line = line.parse::<Line>().unwrap();
-        println!("parsed_line = {:?}", parsed_line);
-        result += parsed_line.value();
+        let value = parsed_line.value();
+        println!("index = {}, line = {}, parsed_line = {:?}, value = {}", index, line, parsed_line, value);
+        result.push(value);
     }
-    result
+    // println!("values = {:?}", result);
+    result.iter().sum()
 }
 
 fn main() {
-    println!("test value = {}", calculate_calibration_value(TEST_STR));
     match std::fs::read_to_string("day1.input") {
-        Ok(input) => println!("real value = {}", calculate_calibration_value(&input)),
+        Ok(input) => println!("value = {}", calculate_calibration_value(&input)),
         Err(reason) => println!("error = {}", reason)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
@@ -83,5 +100,46 @@ mod tests {
         line.visit_digit(5);
         line.visit_digit(3);
         assert_eq!(line.value(), 53);
+    }
+
+    #[test]
+    fn test_match_digit_word_start() {
+        let result = Line::match_digit_word_prefix("two29eighteight1", 0);
+        assert_eq!(result, Some((2, 3)));
+    }
+
+    #[test]
+    fn test_match_digit_word_middle() {
+        let result = Line::match_digit_word_prefix("two29eighteight1", 5);
+        assert_eq!(result, Some((8, 5)));
+    }
+
+    #[test]
+    fn calculate_calibration_value_example1() {
+        static DATA: &str = "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet";
+        let value = calculate_calibration_value(DATA);
+        assert_eq!(142, value);
+    }
+
+    #[test]
+    fn calculate_calibration_value_example2() {
+        static DATA: &str = "two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen";
+        let value = calculate_calibration_value(DATA);
+        assert_eq!(281, value);
+    }
+
+    #[test]
+    fn calculate_calibration_value_oneight() {
+        let result = calculate_calibration_value("oneight");
+        assert_eq!(result, 18);
     }
 }
