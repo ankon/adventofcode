da236df7459bf96cd49e603ff2a9d1abfc215dbf
Andreas Kohn (Framer) <andreas.kohn@framer.com>
2023-12-05 21:39:58 +0100

    Day 4, part 2: Distribute the winnings

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Scratchcard {
     id: u32,
@@ -53,13 +55,18 @@ impl std::str::FromStr for Scratchcard {
 }
 
 impl Scratchcard {
-    fn count_points(&self) -> u32 {
+    fn count_winning(&self) -> u32 {
         let mut winning = 0;
         for number in &self.numbers {
             if self.winning_numbers.contains(number) {
                 winning += 1;
             }
         }
+        winning
+    }
+
+    fn count_points(&self) -> u32 {
+        let winning = self.count_winning();
         if winning == 0 {
             return 0;
         }
@@ -94,6 +101,29 @@ impl PileOfScratchcards {
         }
         points
     }
+
+    fn count_cards_part_2(&self) -> usize {
+        let mut dups = HashMap::<u32, usize>::new();
+        let mut cards = 0;
+        for card in &self.cards {
+            let mut cards_of_id = 1;
+            if let Some(dups_of_id) = dups.get(&card.id) {
+                cards_of_id += dups_of_id;
+            }
+            println!("cards of id {} = {}", card.id, cards_of_id);
+            cards += cards_of_id;
+
+            // Distribute the winning cards further
+            let winning = card.count_winning();
+            println!("winning = {}", winning);
+            for i in 1..=winning {
+                dups.entry(card.id + i)
+                    .and_modify(|e| *e += cards_of_id )
+                    .or_insert(cards_of_id);
+            }
+        }
+        cards
+    }
 }
 
 pub fn main() {
@@ -101,6 +131,7 @@ pub fn main() {
         Ok(input) => {
             if let Ok(scratchcards) = input.parse::<PileOfScratchcards>() {
                 println!("points = {}", scratchcards.count_points());
+                println!("cards = {}", scratchcards.count_cards_part_2());
             }
         },
         Err(reason) => println!("error = {}", reason)
@@ -121,5 +152,17 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         let scratchcards = DATA.parse::<PileOfScratchcards>().unwrap();
         assert_eq!(scratchcards.count_points(), 13);
+    }
+
+    #[test]
+    fn count_cards_part_2() {
+        static DATA: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        let scratchcards = DATA.parse::<PileOfScratchcards>().unwrap();
+        assert_eq!(scratchcards.count_cards_part_2(), 30);
     }
 }
